3acebbf552984abf8c8fd83740a7b8af5568d962
JAmoMES <atipat.pa@ku.th>
2021-07-07 16:54:15 +0700

    change eslin config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,49 @@
 module.exports = {
-  extends: ['airbnb'],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    //You can override any rules you want
+  env: {
+    browser: true,
+    es2021: true
   },
-};
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+      "no-use-before-define": "off",
+      "react/prop-types": "off",
+      "comma-dangle": "off",
+      semi: "off",
+      "space-before-function-paren": "off",
+      "@typescript-eslint/no-var-requires":"off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          ts: "never",
+          tsx: "never"
+        }
+      ],
+      "import/no-duplicates":"off"
+    },
+    settings: {
+      "import/resolver": {
+        // "node":{
+        //   "extensions": [".js", ".jsx", ".ts", ".tsx"]
+        // },
+        typescript: {} 
+      }
+  }
+}
