722c8c2cc4dde407f6b9f9f60f0cc3e400d052a0
Wani29I <pachara31458@gmail.com>
2021-07-29 15:57:08 +0700

    edit lecture.dto

--- src/constants/dto/lecture.dto.ts
+++ src/constants/dto/lecture.dto.ts
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app'
+
+//-------------------------- Lecture -------------------------
+
 export interface postLecture {
   lectureId?: string
   userId: string
@@ -6,33 +10,105 @@ export interface postLecture {
   isMid?: boolean
   isFinal?: boolean
   imagesUrl: string[]
+  keyword: string[]
+  createDate: firebase.firestore.Timestamp
+
+  // no likeCount: number
+  // no viewCount: number
+  // no updateDate: firebase.firestore.Timestamp
   // todo: คณะ ภาค ชื่อวิชา รหัสวิชา
 }
 
-export interface comment {
-  lectureId: string
+export interface editLecture {
+  lectureId?: string
   userId: string
-  displayName: string
-  imageUrl: string
-  message?: string
-  reply?: string
+  lectureTitle?: string
+  description?: string
+  isMid?: boolean
+  isFinal?: boolean
+  imagesUrl: string[]
+  keyword: string[]
+  createDate: firebase.firestore.Timestamp
+  updateDate: firebase.firestore.Timestamp
+  likeCount?: number
+  viewCount?: number
+
+  // todo: คณะ ภาค ชื่อวิชา รหัสวิชา
 }
 
+//-------------------------- review -------------------------
+
 export interface review {
+  reviewId: string
   lectureId: string
   userId: string
   displayName: string
   imageUrl: string
   rating: number
+  message?: string
+  createDate: firebase.firestore.Timestamp
 }
 
+export interface editReview {
+  reviewId: string
+  lectureId: string
+  userId: string
+  displayName: string
+  imageUrl: string
+  rating?: number
+  message?: string
+  createDate: firebase.firestore.Timestamp
+  updateDate: firebase.firestore.Timestamp
+}
+
+//-------------------------- Q&A -------------------------
+
 export interface qAndA {
-  id?: string
+  qaId?: string
   lectureId: string
   userId: string
   displayName: string
   imageUrl: string
   question: string
-  answer?: string
+  answer?: string[]
   status?: number
+  createDate: firebase.firestore.Timestamp
+}
+
+export interface editQAndA {
+  qaId?: string
+  lectureId: string
+  userId: string
+  displayName: string
+  imageUrl: string
+  question: string
+  answer?: string[]
+  status?: number
+  createDate: firebase.firestore.Timestamp
+  updateDate: firebase.firestore.Timestamp
+}
+
+//-------------------------- Comment -------------------------
+
+export interface comment {
+  commentId: string
+  lectureId: string
+  userId: string
+  displayName: string
+  imageUrl: string
+  message?: string
+  reply?: string[]
+  createDate: firebase.firestore.Timestamp
+}
+
+export interface comment {
+  commentId: string
+  lectureId: string
+  userId: string
+  displayName: string
+  imageUrl: string
+  message?: string
+  reply?: string[]
+  createDate: firebase.firestore.Timestamp
+  updateDate: firebase.firestore.Timestamp
 }
