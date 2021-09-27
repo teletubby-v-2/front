14fbe6e3552de0e2d1248c0640d3177facfee362
numkhang <numkhang44@gmail.com>
2021-09-27 18:59:44 +0900

    add following / followers

--- src/pages/FollowList/index.tsx
+++ src/pages/FollowList/index.tsx
@@ -1,25 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { FollowCard } from '../../components/FollowCard'
-import { MyUser } from '../../constants/interface/myUser.interface'
 import { getUserDetial } from '../../service/user'
 
-export const FollowList: React.FC = () => {
-  const { userId } = useParams<{ userId: string }>()
-  const [info, setInfo] = useState({} as MyUser)
+export const FollowList = () => {
+  const { userId, type } = useParams<{ userId: string; type: string }>()
+  const [list, setList] = useState<string[]>([])
+  const [text, settext] = useState<string>('')
   const minRow = 3
 
   //todo:GetuserData
 
   useEffect(() => {
-    getUserDetial(userId).then(data => setInfo({ ...data, userId: data.userId } as MyUser))
+    getUserDetial(userId).then(data => {
+      if (type == 'following') {
+        setList(data.following)
+        settext(data.userName + ' กำลังติดตาม')
+      } else if (type == 'followers') {
+        setList(data.followers)
+        settext(' ผู้ที่กำลังติดตาม ' + data.userName)
+      }
+    })
   }, [userId])
 
   return (
     <div className="mt-10">
-      <div className="text-center text-2xl mb-10">{info.userName} กำลังติดตาม</div>
+      <div className="text-center text-2xl mb-10">{text}</div>
       <div className={`grid gap-y-10 md:grid-cols-4 row-${minRow}-card `}>
-        {info.following?.map(userId => (
+        {list.map(userId => (
           <FollowCard userId={userId} key={userId} className="mx-auto border-2 border-gray-500" />
         ))}
       </div>
