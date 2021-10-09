59f64f76caa34789a0f1f7e90a7c58376261ecd6
JAmoMES <atipat.pa@ku.th>
2021-10-09 18:28:51 +0700

    fix: image url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,9 +10,9 @@ export enum COLLECTION {
 }
 
 export enum SVG_URL {
-  DOG = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/images%2F063769321289.613000000Group%2081dog.svg?alt=media&token=f015b6ce-d06f-46c3-93e0-5fdc3474086e',
-  MAIL = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/images%2F063769321289.613000000Frame%20139.svg?alt=media&token=bb578fe3-e7f8-4cd3-ade1-b1e6caf00468',
-  LOGIN = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/images%2F063769321289.613000000Frame%20148%E0%B8%94%E0%B8%94.svg?alt=media&token=89204097-d3fe-41a5-bc4c-4520cd7704f5',
-  REGISTER = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/images%2F063769321289.613000000Frame%20149register.svg?alt=media&token=ef6d6c23-13ea-40ca-bcc3-855a13fd543a',
-  HOME = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/images%2F063769322063.404000000Frame%20150.svg?alt=media&token=1ddcda87-0ada-45ea-a981-4282907fb491',
+  DOG = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/icons%2FGroup%2081dog.svg?alt=media&token=c2ad4a83-a07b-46ad-a5ba-cf58c882ec38',
+  MAIL = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/icons%2FFrame%20139.svg?alt=media&token=37804e30-182f-4079-970e-26797a096e7b',
+  LOGIN = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/icons%2FFrame%20148%E0%B8%94%E0%B8%94.svg?alt=media&token=d7ce21e2-88fa-4ba9-9a16-cb6a51b4854b',
+  REGISTER = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/icons%2FFrame%20149register.svg?alt=media&token=4b2ad375-e60d-4f8f-bb1a-dcdce174cbe4',
+  HOME = 'https://firebasestorage.googleapis.com/v0/b/teletubby-v2.appspot.com/o/icons%2FFrame%20150.svg?alt=media&token=a7b675bb-385c-4649-a4bf-340730fe855b',
 }
